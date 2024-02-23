1e67bf30d6a0779e9a4d326859aaeaba8a797d05
DejanovicIT42 <73885609+DejanovicIT42@users.noreply.github.com>
2024-02-23 23:44:42 +0100

    White space stuff

--- currency-exchange/src/main/java/currencyExchange/CurrencyExchangeRepository.java
+++ currency-exchange/src/main/java/currencyExchange/CurrencyExchangeRepository.java
@@ -2,7 +2,7 @@ package currencyExchange;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long>{
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
 
-	CurrencyExchange findByFromAndToIgnoreCase(String from, String to);
+    CurrencyExchange findByFromAndToIgnoreCase(String from, String to);
 }
